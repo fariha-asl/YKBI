a47018f6640cf96239fff2c5e09c8f9dd001e25b
Fariha <fariha@asl.aero>
2026-05-18 10:44:12 +0600

    Fix: remove the host

--- files/server.js
+++ files/server.js
@@ -47,7 +47,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅  MongoDB connected");
-    app.listen(PORT,'0.0.0.0',() => console.log(`🚀  Server running on port ${PORT}`));
+    app.listen(PORT,() => console.log(`🚀  Server running on port ${PORT}`));
   })
   .catch((err) => {
     console.error("❌  MongoDB connection error:", err.message);
