37e21d9d572c383d348028ea1f49b60b13191030
fariha-asl <78470903+fariha-asl@users.noreply.github.com>
2026-06-28 14:51:57 +0600

    Update auth.js

--- files/routes/auth.js
+++ files/routes/auth.js
@@ -1,11 +1,9 @@
 const express    = require("express");
 const router     = express.Router();
 const controller = require("../controllers/authController");
-const protect    = require("../middleware/auth");
 
 router.post("/register",        controller.register);
 router.post("/login",           controller.login);
 router.post("/forgot-password", controller.forgotPassword);
-router.get( "/me",      protect, controller.getMe);
 
 module.exports = router;
